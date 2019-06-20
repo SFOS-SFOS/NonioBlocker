1198900ad8a1e5049d0f28055f792f72c9a873cd
SFOS-SFOS <up201603194@fc.up.pt>
2019-06-20 15:31:30 +0100

    Code restyle

---
+++ nonio-remover/popup.js
@@ -0,0 +1,315 @@
+let toRemove = "";
+
+let maquina = document.getElementById('maquina');
+maquina.onclick = function (element) {
+    toRemove = ["gatting_container"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let comercial = document.getElementById('comercial');
+comercial.onclick = function (element) {
+    toRemove = ["nonioBox"];
+    removeClassNonio(toRemove);
+};
+
+let record = document.getElementById('record');
+record.onclick = function (element) {
+    // toRemove=["layer_gatting"];
+    // removeIdNonio(toRemove);
+    toRemove = ["gatting_container"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let jn = document.getElementById('jn');
+jn.onclick = function (element) {
+    toRemove = ["tp-modal", "tp-backdrop"];
+    removeClassNonio(toRemove);
+};
+
+let publico = document.getElementById('publico');
+publico.onclick = function () {
+    toRemove = ["warning-nonio-overlay"];
+    removeClassNonio(toRemove);
+};
+
+//add
+let autoportal = document.getElementById('autoportal');
+autoportal.onclick = function () {
+    toRemove = ["nonioBox"];
+    removeClassNonio(toRemove);
+};
+
+let classificadoscm = document.getElementById('classificadoscm');
+classificadoscm.onclick = function () {
+    // toRemove =["sso_layer"];
+    // removeIdNonio(toRemove);
+    toRemove = ["gatting_container"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let cmjornal = document.getElementById('cmjornal');
+cmjornal.onclick = function () {
+    // toRemove = ["layer_gatting", "sso_layer"];
+    // removeIdNonio(toRemove);
+    toRemove = ["gatting_container"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let dinheirovivo = document.getElementById('dinheirovivo');
+dinheirovivo.onclick = function () {
+    removeIframesNonio();
+};
+
+let dn = document.getElementById('dn');
+dn.onclick = function () {
+    toRemove = ["tp-modal", "tp-backdrop tp-active"];
+    removeClassNonio(toRemove);
+};
+
+let expresso = document.getElementById('expresso');
+expresso.onclick = function () {
+    toRemove = ["imp-content-gate-root"];
+    removeIdNonio(toRemove);
+};
+
+let flash = document.getElementById('flash');
+flash.onclick = function () {
+    // toRemove = ["layer_gatting"];
+    // removeIdNonio(toRemove);
+    toRemove = ["gatting_container"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let jornaldenegocios = document.getElementById('jornaldenegocios');
+jornaldenegocios.onclick = function () {
+    toRemove = ["gatting_container"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let maisfutebol = document.getElementById('maisfutebol');
+maisfutebol.onclick = function () {
+    toRemove = ["nonioBox"];
+    removeClassNonio(toRemove);
+};
+
+let maxima = document.getElementById('maxima');
+maxima.onclick = function () {
+    toRemove = ["gatting_container"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let megahits = document.getElementById('megahits');
+megahits.onclick = function () {
+    toRemove = [""];
+    removeParentClassIdNonio("maskContentGatingNonio");
+};
+
+let motor24 = document.getElementById('motor24');
+motor24.onclick = function () {
+    removeIframesNonio();
+};
+
+let noticiasmagazine = document.getElementById('noticiasmagazine');
+noticiasmagazine.onclick = function () {
+    removeIframesNonio();
+};
+
+let selfie = document.getElementById('selfie');
+selfie.onclick = function () {
+    toRemove = ["nonioBox"];
+    removeClassNonio(toRemove);
+
+};
+
+let smoothfm = document.getElementById('smoothfm');
+smoothfm.onclick = function () {
+    toRemove = ["nonioBox"];
+    removeClassNonio(toRemove);
+};
+
+let tvi = document.getElementById('tvi');
+tvi.onclick = function () {
+    toRemove = ["nonioBox"];
+    removeClassNonio(toRemove);
+};
+
+let tvi24 = document.getElementById('tvi24');
+tvi24.onclick = function () {
+    console.log("Click on TVI24");
+    toRemove = ["nonioBox"];
+    removeClassNonio(toRemove);
+};
+
+let blitz = document.getElementById('blitz');
+blitz.onclick = function () {
+    removeIframesNonio();
+    toRemove = ["_3uC1ta_PlzWRINX9igoXs- brand__blitz"];
+    removeClassNonio(toRemove);
+};
+
+let cidadefm = document.getElementById('cidadefm');
+cidadefm.onclick = function () {
+    toRemove = ["nonioBox"];
+    removeClassNonio(toRemove);
+};
+
+let evasoes = document.getElementById('evasoes');
+evasoes.onclick = function () {
+    removeIframesNonio();
+};
+
+let m80 = document.getElementById('m80');
+m80.onclick = function () {
+    toRemove = ["nonioBox"];
+    removeClassNonio(toRemove);
+};
+
+let renascenca = document.getElementById('renascenca');
+renascenca.onclick = function () {
+    toRemove = ["maskContentGatingNonio"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let tsf = document.getElementById('tsf');
+tsf.onclick = function () {
+    removeIframesNonio();
+};
+
+let rfm = document.getElementById('rfm');
+rfm.onclick = function () {
+    toRemove = ["maskContentGatingNonio"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let sabado = document.getElementById('sabado');
+sabado.onclick = function () {
+    toRemove = ["gatting_containerLN681a8ea163a8a8224a11839ad9e2b731"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let sic = document.getElementById('sic');
+sic.onclick = function () {
+    toRemove = ["_3uC1ta_PlzWRINX9igoXs- brand__sic"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let sicmulher = document.getElementById('sicmulher');
+sicmulher.onclick = function () {
+    toRemove = ["_3uC1ta_PlzWRINX9igoXs- brand__sicmul"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let sicnoticias = document.getElementById('sicnoticias');
+sicnoticias.onclick = function () {
+    toRemove = ["_3uC1ta_PlzWRINX9igoXs- brand__sicnot"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let sicradical = document.getElementById('sicradical');
+sicradical.onclick = function () {
+    toRemove = ["_3uC1ta_PlzWRINX9igoXs- brand__sic"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let sick = document.getElementById('sick');
+sick.onclick = function () {
+    toRemove = ["_3uC1ta_PlzWRINX9igoXs- brand__sic"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let siccaras = document.getElementById('siccaras');
+siccaras.onclick = function () {
+    toRemove = ["_3uC1ta_PlzWRINX9igoXs- brand__sic"];
+    removeParentClassIdNonio(toRemove);
+};
+
+let tviplayer = document.getElementById('tviplayer');
+tviplayer.onclick = function () {
+    toRemove = ["nonioBox"];
+    removeClassNonio(toRemove);
+};
+
+let ojogo = document.getElementById('ojogo');
+ojogo.onclick = function () {
+    toRemove = ["tp-modal", "tp-backdrop"];
+    removeClassNonio(toRemove);
+};
+
+let vidas = document.getElementById('vidas');
+vidas.onclick = function () {
+    toRemove = ["gatting_container"];
+    removeParentClassIdNonio(toRemove);
+};
+
+
+function removeParentClassIdNonio(elemName) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.executeScript(
+            {
+                code: 'document.getElementsByClassName("' + elemName + '")[0].parentElement.remove();'
+            }
+        );
+        ativateScrolsBar();
+    });
+}
+
+
+function removeClass(elemName) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.executeScript(
+            {
+                code: 'document.getElementsByClassName("' + elemName + '")[0].remove();'
+            }
+        );
+        ativateScrolsBar();
+    });
+
+}
+
+function removeClassNonio(remArray) {
+    for (let i = 0; i < remArray.length; i++) {
+        try {
+            removeClass(remArray[i]);
+        } catch (error) {
+            console.log("ERROR: ", error);
+        }
+    }
+}
+
+function removeIdNonio(remArray) {
+    for (let i = 0; i < remArray.length; i++) {
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.executeScript(
+                {
+                    code:
+                        'var element = document.getElementById("' + remArray[i] + '").remove();'
+                }
+            );
+            ativateScrolsBar();
+        });
+    }
+}
+
+function removeIframesNonio() {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.executeScript(null,
+            {
+                code:
+                    'document.querySelectorAll("iframe").forEach(el => el.remove()); ' +
+                    'document.getElementsByTagName("body")[0].style = "overflow:auto !important"; ' +
+                    'document.getElementsByTagName("html")[0].style = "overflow:auto !important"; '
+            }
+        );
+    });
+}
+
+function ativateScrolsBar() {
+    chrome.tabs.executeScript(
+        {
+            code:
+                'document.getElementsByTagName("body")[0].style = "overflow:auto !important"; ' +
+                'document.getElementsByTagName("html")[0].style = "overflow:auto !important";'
+        }
+    );
+
+}
