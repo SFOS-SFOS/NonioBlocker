810495e0a3f019443da07768c1f1e2598dd64835
Simão Silva <37107350+SFOS-SFOS@users.noreply.github.com>
2019-06-25 16:28:08 +0100

    Small fix

--- nonio-remover/background.js
+++ nonio-remover/background.js
@@ -1,114 +1,59 @@
-console.log("Entrei 2");
+// https://gist.github.com/prasanthj/a5b71c47ee0dec3bbb72
+// https://gist.github.com/jlong/2428561#file-uri-js
+
+
+// chrome.runtime.onInstalled.addListener(function () {
+//     try {
+//         let thisVersion = chrome.runtime.getManifest().version;
+//         console.log("Version: ", thisVersion);
+//         if (details.reason === "install") {
+//             console.info("First version installed");
+//             alert("Remover Nonio foi instalado pela primeira vez :)");
+//             //Send message to popup.html and notify/alert user("Welcome")
+//         } else if (details.reason === "update") {
+//             console.info("Remover Nonio atializado para a versão: " + thisVersion);
+//             //Send message to popup.html and notify/alert user
+//
+//             chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+//                 for (var i = 0; i < tabs.length; i++) {
+//                     chrome.tabs.sendMessage(tabs[i].id, {name: "showPopupOnUpdated", version: thisVersion});
+//                 }
+//             });
+//         }
+//     } catch (e) {
+//         console.info("OnInstall Error - " + e);
+//     }
+// });
+
 
 let tabID = null;
 
-
-// https://gist.github.com/jlong/2428561#file-uri-js
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    console.log("ID: ", tabId);
-    console.log("Info: ", changeInfo);
-    console.log("Tab: ", tab);
+
+    // console.log("ID: ", tabId);
+    // console.log("Info: ", changeInfo);
+    // console.log("Tab: ", tab);
 
     if (changeInfo.status === "complete") {
         let url = tab.url;
         let parser = document.createElement("a");
         parser.href = url;
 
-        console.log("URL: ", url);
-        console.log("Parser: ", parser.hostname);
+        // console.log("URL: ", url);
+        // console.log("Parser: ", parser.hostname);
 
-        tabID = tabId;
+        // tabID = tab.index;
+        // console.log("Index: ", tab.index);
+        // console.log("Index2 : ", tab.id);
+
         removeNonio(parser.hostname);
     }
 
 });
 
-chrome.runtime.onInstalled.addListener(function () {
-    try {
-        let thisVersion = chrome.runtime.getManifest().version;
-        console.log("Version: ", thisVersion);
-        if (details.reason === "install") {
-            console.info("First version installed");
-            alert("Remover Nonio foi instalado pela primeira vez :)");
-            //Send message to popup.html and notify/alert user("Welcome")
-        } else if (details.reason === "update") {
-            console.info("Remover Nonio atializado para a versão: " + thisVersion);
-            //Send message to popup.html and notify/alert user
-
-            chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-                for (var i = 0; i < tabs.length; i++) {
-                    chrome.tabs.sendMessage(tabs[i].id, {name: "showPopupOnUpdated", version: thisVersion});
-                }
-            });
-        }
-    } catch (e) {
-        console.info("OnInstall Error - " + e);
-    }
-});
-
-
-function removeParentClassIdNonio(elemName) {
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.executeScript({
-            code: 'document.getElementsByClassName("' + elemName + '")[0].parentElement.remove();'
-        });
-        ativateScrolsBar();
-    });
-}
-
-
-function removeClass(elemName) {
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.executeScript({
-            code: 'document.getElementsByClassName("' + elemName + '")[0].remove();'
-        });
-        ativateScrolsBar();
-    });
-
-}
-
-function removeClassNonio(remArray) {
-    for (let i = 0; i < remArray.length; i++) {
-        try {
-            removeClass(remArray[i]);
-        } catch (error) {
-            console.log("ERROR: ", error);
-        }
-    }
-}
-
-
-function removeIdNonio(remArray) {
-    for (let i = 0; i < remArray.length; i++) {
-        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            chrome.tabs.executeScript({
-                code: 'document.getElementById("' + remArray[i] + '").remove();'
-            });
-            ativateScrolsBar();
-        });
-    }
-}
-
-function removeIframesNonio() {
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.executeScript(null, {
-            code: 'document.querySelectorAll("iframe").forEach(e => e.remove());'
-        });
-        ativateScrolsBar();
-    });
-}
-
-function ativateScrolsBar() {
-    chrome.tabs.executeScript({
-        code:
-            'document.getElementsByTagName("body")[0].style = "overflow:auto !important"; ' +
-            'document.getElementsByTagName("html")[0].style = "overflow:auto !important";'
-    });
-}
-
 
 function removeNonio(hostname) {
-    console.log("Vou remover de ", hostname);
+    // console.log("Vou remover de ", hostname);
 
     switch (hostname) {
 
@@ -195,4 +140,64 @@ function removeNonio(hostname) {
             removeParentClassIdNonio(["_3uC1ta_PlzWRINX9igoXs- brand__sicnot"]);
             break;
     }
+}
+
+
+function removeParentClassIdNonio(elemName) {
+    chrome.tabs.query({active: true, currentWindow: true}, function () {
+        chrome.tabs.executeScript({
+            code: 'document.getElementsByClassName("' + elemName + '")[0].parentElement.remove();'
+        });
+        ativateScrolsBar();
+    });
+}
+
+
+function removeClass(elemName) {
+    chrome.tabs.query({active: true, currentWindow: true}, function () {
+        chrome.tabs.executeScript({
+            code: 'document.getElementsByClassName("' + elemName + '")[0].remove();'
+        });
+        ativateScrolsBar();
+    });
+
+}
+
+function removeClassNonio(remArray) {
+    for (let i = 0; i < remArray.length; i++) {
+        try {
+            removeClass(remArray[i]);
+        } catch (error) {
+            console.log("ERROR: ", error);
+        }
+    }
+}
+
+
+function removeIdNonio(remArray) {
+    for (let i = 0; i < remArray.length; i++) {
+        chrome.tabs.query({active: true, currentWindow: true}, function () {
+            chrome.tabs.executeScript({
+                code: 'document.getElementById("' + remArray[i] + '").remove();'
+            });
+            ativateScrolsBar();
+        });
+    }
+}
+
+function removeIframesNonio() {
+    chrome.tabs.query({active: true, currentWindow: true}, function () {
+        chrome.tabs.executeScript(null, {
+            code: 'document.querySelectorAll("iframe").forEach(e => e.remove());'
+        });
+        ativateScrolsBar();
+    });
+}
+
+function ativateScrolsBar() {
+    chrome.tabs.executeScript({
+        code:
+            'document.getElementsByTagName("body")[0].style = "overflow:auto !important"; ' +
+            'document.getElementsByTagName("html")[0].style = "overflow:auto !important";'
+    });
 }
